4f5f75cb34e93a1331c1f01bbe6f6c0aa44966ac
root <root@b22908c43778.vp-s.cloud>
2025-03-11 21:36:32 +0000

    update halving

--- src/validation.cpp
+++ src/validation.cpp
@@ -1171,7 +1171,7 @@ CAmount GetBlockSubsidy(int nPrevBits, int nPrevHeight, const Consensus::Params&
         return 0;
 
     CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    // Subsidy is cut in half every 262,500 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
     
